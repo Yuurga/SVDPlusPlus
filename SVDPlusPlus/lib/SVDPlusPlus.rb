9d91c5573910adb1e261ce9aec541f1c061a5e07
Yuurga <yuurga@gmail.com>
2013-07-08 19:04:29 +0400

    Class variables

--- SVDPlusPlus/lib/SVDPlusPlus.rb
+++ SVDPlusPlus/lib/SVDPlusPlus.rb
@@ -7,12 +7,43 @@ module SVDPlusPlus
   class SvdPlusPlus 
   
   
+  
 	# Create a new SVD Matrix with m(Users) rows, n(goods,topics etc) columns, 
 	def initialize(m,n)
 		@m=m
 		@n=n
 		@tdm=SVDMatrix.new(m,n) 
+		
+		#Results of singular value decomposition of matrix tdm
+		@u=nil
+		@w=nil
+		@v=nil
+		
+		#middle rating for all users\goods
+		@mu_=0
+		
+		# vector b_u
+		@b_u_=nil
+		
+		# vector b_v
+		@b_v_=nil
+		
+		#matrix q
+		@q_=nil
+		
+		#matrix p
+		@p_=nil
+    
 	end
+	
+	#Multiplication of vectors v[a] *u[i]
+	def dot(v,u,a,i)
+    res = 0
+    for j in 0..1
+      res+=v[a,j]*u[i,j]
+    end
+    return res
+  end
 	
   end
 end
