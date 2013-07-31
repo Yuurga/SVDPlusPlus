3609d6c81427f8391f559697b215ee822a27f564
Yuurga <yuurga@gmail.com>
2013-07-31 15:43:18 +0400

    add  function learn, get prediction

--- SVDPlusPlus/lib/SVDPlusPlus.rb
+++ SVDPlusPlus/lib/SVDPlusPlus.rb
@@ -57,5 +57,105 @@ module SVDPlusPlus
 		return res
 	end
 	
+	
+	def learn()
+		res=tdm.decompose(2)
+		
+		@u=res[0]
+		@w=res[1]
+		@v=res[2]
+    
+		max_v=@n
+		max_u=@m
+		total = max_u*max_v
+	
+    
+		b_u = Array.new(max_u) {0}
+		b_v = Array.new(max_v) {0}
+
+		q = SVDMatrix.new(@n, 2)
+		p = SVDMatrix.new(@m, 2)
+    
+		# q nx2
+		for i in 0..@n-1 do
+		  for j in 0..1 do
+			q[i,j]=0.000001
+		  end
+		end
+		
+		# p mx2
+		for i in 0..@m-1 do
+		  for j in 0..1 do
+			p[i,j]=0.000001*j
+		  end
+		end
+		
+		#prepared continue
+		eps_u=1
+		eps_v=1
+		iter=0
+		eta=0.05
+		mu=0.0
+		lambda=0.1
+		lambda2=0.1
+		
+		err = 0.0;
+		rmse = 1;
+		old_rmse = 0;
+		
+		threshold = 0.01;
+		
+		#learning cycle
+		while ([eps_u,eps_v].max > 0.000001 ) do
+
+			b_u_old=Array.new(b_u.length) {|id| b_u[id]}
+			b_v_old=Array.new(b_v.length) {|id| b_v[id]}
+			
+			for i in 0..@m-1 do
+			  for a in 0..@n-1 do
+				err=tdm[i,j]-mu-b_u[i]-b_v[a]-dot(@v,@u,a,i)
+				
+				mu = err * eta
+				b_u[i]=b_u[i]+eta * (err - lambda * b_u[i])
+				b_v[a]=b_u[a]+eta * (err - lambda * b_u[a])
+				
+				for j in 0..1 do
+				  q[a,j]=q[a,j]+eta*(err*p[i,j]-lambda2*q[a,j])
+				  p[i,j]=p[i,j]+eta*(err*q[a,j]-lambda2*p[i,j])
+				end
+				
+			  end 
+			end
+			eps_u=get_eps(b_u,b_u_old)
+			eps_v=get_eps(b_v,b_v_old)
+			iter+=1
+		end
+		  
+		puts iter
+		@mu_=mu
+		@b_u_=b_u
+		@b_v_=b_v
+		@q_=q
+		@p_=p
+	
+	end
+	
+	#return prediction for user i for all items
+	def get_prediction(i)
+		res=[]
+		for j in 0..9
+			bu=@b_u_[i]
+			
+			bv=@b_v_[j]
+			
+			mult=dot(@p_,@q_,i,j)
+			#mult=0 if mult < 0
+			ver=@mu+bu+bv+mult
+			ver=2 if ver > 2
+			  res << ver
+		end
+		return res
+  end
+	
   end
 end
